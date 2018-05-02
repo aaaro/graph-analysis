b2f2075efd798f2f095f897944c89b57012f869a
Unknown <wiktor.elattar@gmail.com>
2018-05-02 19:16:46 +0200

    Added models

---
+++ Wyszukiwanie_mostów_w_grafie/Models/Vertex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyszukiwanie_mostów_w_grafie.Models
+{
+    class Vertex
+    {
+        ushort value;
+        public ushort Value { get { return value; } }
+
+        public Vertex(ushort value)
+        {
+            this.value = value;
+        }
+    }
+}
---
+++ Wyszukiwanie_mostów_w_grafie/Models/Verticle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyszukiwanie_mostów_w_grafie.Models
+{
+    class Verticle
+    {
+        Vertex u;
+        Vertex v;
+        bool directed;
+
+        public Verticle(Vertex u, Vertex v, bool directed)
+        {
+            this.u = u;
+            this.v = v;
+            this.directed = directed;
+        }
+    }
+}
