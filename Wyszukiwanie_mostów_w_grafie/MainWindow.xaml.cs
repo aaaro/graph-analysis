9fb275b460b75f09bd282c5d7e4a268a0c67f623
kambar13 <36968744+kambar13@users.noreply.github.com>
2018-05-03 20:48:19 +0200

    Pierwsze próby RightClick i opis problemów

--- Wyszukiwanie_mostów_w_grafie/MainWindow.xaml.cs
+++ Wyszukiwanie_mostów_w_grafie/MainWindow.xaml.cs
@@ -21,11 +21,13 @@ namespace Wyszukiwanie_mostów_w_grafie
     public partial class MainWindow : Window
     {
         int n = 0;
+        Queue<int> skasowaneWierchulki = new Queue<int>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        public void DrawSpace_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        public void DrawSpace_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) //LEWY
         {
             n++;
             Ellipse square = new Ellipse
@@ -35,26 +37,71 @@ namespace Wyszukiwanie_mostów_w_grafie
                 Fill = Brushes.White,
                 Stroke = Brushes.Black,
                 StrokeThickness = 2
-
             };
             TextBox number = new TextBox
             {
-
-                Width = 40,
-                Height = 40,
+                Width = 50,
+                Height = 50,
                 Background = Brushes.Transparent,
                 BorderThickness = new Thickness(0, 0, 0, 0),
                 Text = Convert.ToString(n),
                 FontSize = 30,
-                TextAlignment = TextAlignment.Center
+                TextAlignment = TextAlignment.Center,
+                Padding = new Thickness(0, 3, 0, 0),
+                IsReadOnly = true,
+                Cursor = Cursors.Pen,
             };
+            
             DrawSpace.Children.Add(square);
             DrawSpace.Children.Add(number);
             Canvas.SetLeft(square, e.GetPosition(DrawSpace).X - 25);
             Canvas.SetTop(square, e.GetPosition(DrawSpace).Y - 25);
-            Canvas.SetLeft(number, e.GetPosition(DrawSpace).X - 20);
-            Canvas.SetTop(number, e.GetPosition(DrawSpace).Y - 20);
+            Canvas.SetLeft(number, e.GetPosition(DrawSpace).X - 25);
+            Canvas.SetTop(number, e.GetPosition(DrawSpace).Y - 25);
         }
-        
+
+
+        // tam na dole działa, ale słabo
+        // nie da się kliknąć na objekt, więc może dodatkowy Canvas na górze, przezroczysty?
+        // @up. Jeśli kliknę na objekt (np textbox) to nie klikam na canvas, więc nie łapie MouseRightClick
+        // foreach wywala po zmieanie zawartości DrawSpace.Children, trzeba for i panować nad 'i'
+        // obecny kod kasuje pojedynczo, jeśli kliknie się prawym trochę wyżej lub niżej. I działa, ale nie każdy klik łapie, co jest zjebane, zobaczyć
+        // potencjał jest, ale wymaga przesiedzenia
+
+        public void DrawSpace_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        { }// pusta metoda żeby nie wywalało, bo Canvas ma zadeklarowany RightClick. Do wyjebania przy pracy
+
+        /*
+
+        public void DrawSpace_MouseRightButtonDown(object sender, MouseButtonEventArgs e) //PRAWY
+        {
+            double kursorX = e.GetPosition(DrawSpace).X;
+            double kursorY = e.GetPosition(DrawSpace).Y;
+
+            ZnajdzWierchulki(kursorX, kursorY);
+        }
+        public void ZnajdzWierchulki(double x, double y)
+        {
+            double top = y + 70;
+            double bot = y - 70;
+            double left = x - 25;
+            double right = x + 25;
+
+            foreach (UIElement item in DrawSpace.Children)
+            {
+                double objX = Canvas.GetLeft(item);
+                double objY = Canvas.GetTop(item);
+
+                if (objX > left && objX < right && objY > bot && objY < top)
+                {
+                    DrawSpace.Children.Remove(item);
+                    break;
+
+                }
+            }
+        }
+        */
+
+
     }
 }
