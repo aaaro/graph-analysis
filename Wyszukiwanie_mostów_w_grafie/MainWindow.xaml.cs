a30d53bb289177266d5ed05a848b606dbf0a8476
aaaro <aaaro@op.pl>
2018-05-04 14:00:21 +0200

    Uporządkowanie klas i dodanie modeli

--- Wyszukiwanie_mostów_w_grafie/MainWindow.xaml.cs
+++ Wyszukiwanie_mostów_w_grafie/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,88 +21,26 @@ namespace Wyszukiwanie_mostów_w_grafie
     /// </summary>
     public partial class MainWindow : Window
     {
-        int n = 0;
-        Queue<int> skasowaneWierchulki = new Queue<int>();
-
         public MainWindow()
         {
             InitializeComponent();
         }
-        public void DrawSpace_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) //LEWY
+        //Wciśnięcie lewego przycisku myszy na canvasie
+        public void DrawSpace_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            n++;
-            Ellipse square = new Ellipse
-            {
-                Width = 50,
-                Height = 50,
-                Fill = Brushes.White,
-                Stroke = Brushes.Black,
-                StrokeThickness = 2
-            };
-            TextBox number = new TextBox
-            {
-                Width = 50,
-                Height = 50,
-                Background = Brushes.Transparent,
-                BorderThickness = new Thickness(0, 0, 0, 0),
-                Text = Convert.ToString(n),
-                FontSize = 30,
-                TextAlignment = TextAlignment.Center,
-                Padding = new Thickness(0, 3, 0, 0),
-                IsReadOnly = true,
-                Cursor = Cursors.Pen,
-            };
-            
-            DrawSpace.Children.Add(square);
-            DrawSpace.Children.Add(number);
-            Canvas.SetLeft(square, e.GetPosition(DrawSpace).X - 25);
-            Canvas.SetTop(square, e.GetPosition(DrawSpace).Y - 25);
-            Canvas.SetLeft(number, e.GetPosition(DrawSpace).X - 25);
-            Canvas.SetTop(number, e.GetPosition(DrawSpace).Y - 25);
+            Vertex w = new Vertex();
+            //Dodanie do klasy Vertex eventu MouseRightButtonDown
+            w.MouseRightButtonDown += W_MouseRightButtonDown;
+            DrawSpace.Children.Add(w);
+            Canvas.SetLeft(w, e.GetPosition(DrawSpace).X - 25);
+            Canvas.SetTop(w, e.GetPosition(DrawSpace).Y - 25);
         }
 
-
-        // tam na dole działa, ale słabo
-        // nie da się kliknąć na objekt, więc może dodatkowy Canvas na górze, przezroczysty?
-        // @up. Jeśli kliknę na objekt (np textbox) to nie klikam na canvas, więc nie łapie MouseRightClick
-        // foreach wywala po zmieanie zawartości DrawSpace.Children, trzeba for i panować nad 'i'
-        // obecny kod kasuje pojedynczo, jeśli kliknie się prawym trochę wyżej lub niżej. I działa, ale nie każdy klik łapie, co jest zjebane, zobaczyć
-        // potencjał jest, ale wymaga przesiedzenia
-
-        public void DrawSpace_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
-        { }// pusta metoda żeby nie wywalało, bo Canvas ma zadeklarowany RightClick. Do wyjebania przy pracy
-
-        /*
-
-        public void DrawSpace_MouseRightButtonDown(object sender, MouseButtonEventArgs e) //PRAWY
+        //Wciśnięcie prawego przycisku myszy na narysowanym wierzchołku
+        private void W_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
-            double kursorX = e.GetPosition(DrawSpace).X;
-            double kursorY = e.GetPosition(DrawSpace).Y;
-
-            ZnajdzWierchulki(kursorX, kursorY);
+            Canvas c = sender as Canvas;
+            DrawSpace.Children.Remove(c);
         }
-        public void ZnajdzWierchulki(double x, double y)
-        {
-            double top = y + 70;
-            double bot = y - 70;
-            double left = x - 25;
-            double right = x + 25;
-
-            foreach (UIElement item in DrawSpace.Children)
-            {
-                double objX = Canvas.GetLeft(item);
-                double objY = Canvas.GetTop(item);
-
-                if (objX > left && objX < right && objY > bot && objY < top)
-                {
-                    DrawSpace.Children.Remove(item);
-                    break;
-
-                }
-            }
-        }
-        */
-
-
     }
 }
---
+++ Wyszukiwanie_mostów_w_grafie/Models/Vertex.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Wyszukiwanie_mostów_w_grafie
+{
+    public class Vertex : Canvas
+    {
+        static int count = 0; //licznik wierzchołków
+        public Ellipse ellipse { get; set; } //rysowana elipsa
+        public TextBox textBox { get; set; } //rysowany numer wierzchołka
+
+        public List<Vertex> Neighbours; //Lista sąsaidujących wierzchołków
+
+        public Vertex()
+        {
+            //Deklaracje
+            count++;
+            ellipse = new Ellipse();
+            textBox = new TextBox();
+            Neighbours = new List<Vertex>();
+            //canvas
+            Width = 50;
+            Height = 50;
+            //elipsa
+            ellipse.Width = 50;
+            ellipse.Height = 50;
+            ellipse.Fill = Brushes.White;
+            ellipse.Stroke = Brushes.Black;
+            ellipse.StrokeThickness = 2;
+            Children.Add(ellipse);
+            //textbox
+            textBox.Width = 50;
+            textBox.Height = 50;
+            textBox.Background = Brushes.Transparent;
+            textBox.BorderThickness = new Thickness(0, 0, 0, 0);
+            textBox.Text = Convert.ToString(count);
+            textBox.FontSize = 30;
+            textBox.TextAlignment = TextAlignment.Center;
+            textBox.Padding = new Thickness(0, 3, 0, 0);
+            textBox.IsReadOnly = true;
+            textBox.Cursor = Cursors.Pen;
+            textBox.IsHitTestVisible = false;
+            Children.Add(textBox);          
+        }
+    }
+}
